6bbc84ff10e2b9f567e6229702e9b2a19ff9ae19
wyuefzhuol <45565676+wyuefzhuol@users.noreply.github.com>
2020-07-22 22:09:22 +0800

    java-oo [Green]，finished the test on practice06，according to read.me Requirements complete the  test and pass.

--- src/main/java/practice06/Teacher.java
+++ src/main/java/practice06/Teacher.java
@@ -1,4 +1,34 @@
 package practice06;
 
-public class Teacher {
+public class Teacher extends Person{
+    public int getKlass() {
+        return klass;
+    }
+
+    public void setKlass(int klass) {
+        this.klass = klass;
+    }
+
+    private int klass;
+    public Teacher(String name, int age ,int kclass) {
+        super(name, age);
+        this.klass = kclass;
+    }
+    public Teacher(String name, int age) {
+        super(name, age);
+        this.klass = -1;
+    }
+    @Override
+    public String introduce() {
+        //My name is Tom. I am 21 years old. I am a Teacher. I teach Class 2.
+        String line = "";
+        if(klass!=-1){
+            line = super.introduce() + " I am a Teacher. I teach Class "+klass+".";
+        }else {
+            //My name is Tom. I am 21 years old.
+            line = super.introduce() + " I am a Teacher. I teach No Class.";
+        }
+        return line;
+    }
+
 }
