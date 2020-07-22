966019e1cd54ab2e77dc1272cd855cfb7acf2dff
wyuefzhuol <45565676+wyuefzhuol@users.noreply.github.com>
2020-07-22 22:16:07 +0800

    java-oo [Green]，feature all functions ，According to the requirements, all the functions are written and passed the test.

--- src/main/java/practice07/Klass.java
+++ src/main/java/practice07/Klass.java
@@ -1,4 +1,19 @@
 package practice07;
 
 public class Klass {
+    public int getNumber() {
+        return number;
+    }
+
+    public void setNumber(int number) {
+        this.number = number;
+    }
+
+    private  int number;
+    public Klass(int number) {
+        this.number = number;
+    }
+    public String getDisplayName(){
+        return "Class "+number;
+    }
 }
